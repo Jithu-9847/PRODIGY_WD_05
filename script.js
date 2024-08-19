87f8f12bc57bb51859dcc7f6c81fe4c4eeefee7d
Jithu-9847 <131839320+Jithu-9847@users.noreply.github.com>
2024-08-19 23:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
-const apiKey = 'de09e4674a1144d890c75753241108';
+const apiKey = 'b12d28443eef453a97dcee249c0945f4';
 const form = document.getElementById('city-form');
 const temperatureElement = document.getElementById('temperature');
 const conditionElement = document.getElementById('condition');
 const weatherIconElement = document.getElementById('weather-icon');
 
+async function getWeather(city) {
+    const url = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${apiKey}`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
+        const temperature = `${data.data[0].temp}°F`;
+        const description = data.data[0].weather.description;
+        const iconCode = data.data[0].weather.icon;
+        const iconUrl = `https://www.weatherbit.io/static/img/icons/${iconCode}.png`;
 
-function getWeather(city) {
-  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-
-  fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-
-      const temperature = `${data.current.temp_c}°C`;
-      const description = data.current.condition.text;
-      const iconUrl = `http:${data.current.condition.icon}`;
-
-
-      temperatureElement.textContent = temperature;
-      conditionElement.textContent = description;
-      weatherIconElement.src = iconUrl;
-      weatherIconElement.style.display = 'block';
-    })
-    .catch(error => {
-
-      console.error('There has been a problem with your fetch operation:', error);
-      temperatureElement.textContent = '';
-      conditionElement.textContent = 'Unable to fetch weather data. Please try again.';
-      weatherIconElement.style.display = 'none';
-    });
+        temperatureElement.textContent = temperature;
+        conditionElement.textContent = description;
+        weatherIconElement.src = iconUrl;
+        weatherIconElement.style.display = 'block';
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        temperatureElement.textContent = '';
+        conditionElement.textContent = 'Unable to fetch weather data. Please try again.';
+        weatherIconElement.style.display = 'none';
+    }
 }
+
 document.getElementById("fetch_btn").addEventListener("click", () => {
-
-  const cityInput = document.getElementById('city-input').value;
-  getWeather(cityInput)
-
-})
+    const cityInput = document.getElementById('city-input').value;
+    getWeather(cityInput);
+});
